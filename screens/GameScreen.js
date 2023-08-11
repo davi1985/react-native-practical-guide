b0c30719a98f0437c715556777b7f64dcae0fd9d
Davi Silva <davi.psilva@midway.com.br>
2023-08-11 08:41:40 -0300

    starting game screen component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,1 +1,24 @@
-export const GameScreen = () => {};
+import { StyleSheet, Text, View } from "react-native";
+import { Title } from "../components/Title";
+
+export const GameScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+
+      <View>
+        <Text>Hight or lower ?</Text>
+        {/* + _ */}
+      </View>
+
+      <View>{/* Logs */}</View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
