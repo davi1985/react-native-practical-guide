64985b96f0cb11dbc250ac986a17f789e4c0cdb2
Davi Silva <davi.psilva@midway.com.br>
2023-08-10 21:19:42 -0300

    handling user input

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,26 +1,20 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-export const PrimaryButton = ({ children }) => {
-  const pressHandler = () => {
-    console.log("pressed");
-  };
-
-  return (
-    <View style={styles.buttonOuterContainer}>
-      <Pressable
-        style={({ pressed }) =>
-          pressed
-            ? [styles.buttonInnerContainer, styles.pressed]
-            : styles.buttonInnerContainer
-        }
-        onPress={pressHandler}
-        android_ripple={{ color: "#640233" }}
-      >
-        <Text style={styles.buttonText}>{children}</Text>
-      </Pressable>
-    </View>
-  );
-};
+export const PrimaryButton = ({ children, onPress }) => (
+  <View style={styles.buttonOuterContainer}>
+    <Pressable
+      style={({ pressed }) =>
+        pressed
+          ? [styles.buttonInnerContainer, styles.pressed]
+          : styles.buttonInnerContainer
+      }
+      onPress={onPress}
+      android_ripple={{ color: "#640233" }}
+    >
+      <Text style={styles.buttonText}>{children}</Text>
+    </Pressable>
+  </View>
+);
 
 const styles = StyleSheet.create({
   buttonOuterContainer: {
@@ -31,7 +25,7 @@ const styles = StyleSheet.create({
   buttonInnerContainer: {
     backgroundColor: "#72063c",
     paddingVertical: 8,
-    paddingHorizontal: 6,
+    paddingHorizontal: 16,
     elevation: 2,
   },
   buttonText: {
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,26 +1,77 @@
-import { StyleSheet, TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View, Alert } from "react-native";
 import { PrimaryButton } from "../components/PrimaryButton";
+import { useState } from "react";
+
+const MIN_NUMBER_VALUE = 0;
+const MAX_NUMBER_VALUE = 99;
 
 export const StartGameScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  const numberInputHandler = (enteredText) => {
+    setEnteredNumber(enteredText);
+  };
+
+  const resetInputHandler = () => setEnteredNumber("");
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (
+      isNaN(chosenNumber) ||
+      chosenNumber <= MIN_NUMBER_VALUE ||
+      chosenNumber > MAX_NUMBER_VALUE
+    ) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99",
+        [
+          {
+            text: "Okay",
+            style: "destructive",
+            onPress: resetInputHandler,
+          },
+        ]
+      );
+
+      return;
+    }
+
+    console.log("Valid number - ", chosenNumber);
+  };
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
         style={styles.numberInput}
         maxLength={2}
         keyboardType="number-pad"
+        autoCorrect={false}
+        autoCapitalize="none"
+        value={enteredNumber}
+        onChangeText={numberInputHandler}
       />
 
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainter}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+        </View>
+
+        <View style={styles.buttonContainter}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
     marginTop: 100,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     marginHorizontal: 24,
     borderRadius: 8,
     elevation: 4,
@@ -42,5 +93,11 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainter: {
+    flex: 1,
   },
 });
