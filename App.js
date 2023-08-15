655671fa3bdb2335366a6303acc2fa9f84f3448c
Davi Silva <davi.psilva@midway.com.br>
2023-08-15 20:42:28 -0300

    start navigation

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
+import { AppRegistry, Platform } from "react-native";
+import { registerRootComponent } from "expo";
+import "react-native-gesture-handler";
+import { name as appName } from "./app.json";
+
 import { StatusBar } from "expo-status-bar";
 import { CategoriesScreen } from "./src/screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
+}
+
+if (Platform.OS === "android") {
+  registerRootComponent(App);
+} else {
+  AppRegistry.registerComponent(appName, () => App);
 }
